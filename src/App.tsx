a686d54eeca4e043e0e30b676ffa12a43fb44fba
oliveirajne <oliveirajne@gmail.com>
2020-10-11 18:27:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { View } from 'react-native';
+
+import Routes from './routes';
+import { AuthProvider } from './contexts/auth';
+
+const App: React.FC = () => {
+
+    return (
+        <NavigationContainer>
+            <AuthProvider>
+            
+                <Routes />
+                
+            </AuthProvider>
+        </NavigationContainer>
+        
+    )
+};
+
+export default App;
